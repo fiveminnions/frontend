4a85d18afc53f1f8e444882d09af42cff5ae4be9
fiveminnions <joseph.vargeesh@verizon.com>
2018-07-02 14:05:06 +0000

    New tabs added

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import {Tabs, Tab} from 'react-bootstrap'
 
 import { userActions } from '../_actions';
+import ExpandRow from '../JobSearch/JobSearch'
 
 class HomePage extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    
+        this.handleSelect = this.handleSelect.bind(this);
+        this.check = this.check.bind(this);
+    
+        this.state = {
+          key: 1
+        };
+      }
+    
+      handleSelect(key) {
+        alert(`selected ${key}`);
+        this.setState({ key });
+      }
+      check(){
+          console.log("A");
+      }
     componentDidMount() {
         this.props.dispatch(userActions.getAll());
     }
@@ -16,8 +36,22 @@ class HomePage extends React.Component {
     render() {
         const { user, users } = this.props;
         return (
-            <div className="col-md-6 col-md-offset-3">
-              
+            <div>
+                <Tabs
+                    activeKey={this.state.key}
+                    onSelect={this.handleSelect}
+                    id="controlled-tab-example"
+                >
+                    <Tab eventKey={1} title="Job Search">
+                        <ExpandRow/>
+                </Tab>
+                    <Tab eventKey={2} title="Jobs Applied">
+                        Tab 2 content
+        </Tab>
+                    <Tab eventKey={3} title="Contact Us" disabled>
+                        Tab 3 content
+        </Tab>
+                </Tabs>
                 <p>
                     <Link to="/login">Logout</Link>
                 </p>
