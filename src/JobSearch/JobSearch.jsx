df86824bd3ab585312619f9969997e03c9e63f88
fiveminnions <joseph.vargeesh@verizon.com>
2018-07-03 08:54:02 +0000

    added panel items

--- src/JobSearch/JobSearch.jsx
+++ src/JobSearch/JobSearch.jsx
@@ -1,87 +1,107 @@
 import React from 'react';
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import * as bootstrap from 'react-bootstrap';
+import './dataTable.css'
+import { loadavg } from 'os';
 
 const products = [];
 
-function addProducts(quantity) {
-  const startId = products.length;
-  for (let i = 0; i < quantity; i++) {
-    const id = startId + i;
-    if (i < 3) {
-      products.push({
-        id: id,
-        name: 'Item name ' + id,
-        price: 2100 + i,
-        expand: [ {
-          fieldA: 'test1',
-          fieldB: (i + 1) * 99,
-          fieldC: (i + 1) * Math.random() * 100,
-          fieldD: '123eedd' + i
-        }, {
-          fieldA: 'test2',
-          fieldB: i * 99,
-          fieldC: i * Math.random() * 100,
-          fieldD: '123eedd' + i
-        } ]
-      });
-    } else {
-      products.push({
-        id: id,
-        name: 'Item name ' + id,
-        price: 2100 + i
-      });
-    }
-  }
-}
-addProducts(5);
-
-class BSTable extends React.Component {
-  render() {
-    if (this.props.data) {
-      return (
-        <BootstrapTable data={ this.props.data }>
-          <TableHeaderColumn dataField='fieldA' isKey={ true }>Field A</TableHeaderColumn>
-          <TableHeaderColumn dataField='fieldB'>Field B</TableHeaderColumn>
-          <TableHeaderColumn dataField='fieldC'>Field C</TableHeaderColumn>
-          <TableHeaderColumn dataField='fieldD'>Field D</TableHeaderColumn>
-        </BootstrapTable>);
-    } else {
-      return (<p>?</p>);
-    }
-  }
+const jobsApplied = {
+  jobs: [{
+    jobID: "1",
+    Designation: 'Band 4',
+    SkillSet: 'java , Spring boot',
+    Location: 'OTP- Chennai'
+  },
+  {
+    jobID: "2",
+    Designation: 'Band 5',
+    SkillSet: 'javaScript , React',
+    Location: 'OTP- Chennai'
+  }]
 }
 
-export default class SingleExpandRow extends React.Component {
+export default class ExpandRow extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      open: true
+
+    };
   }
 
-  isExpandableRow(row) {
-    if (row.id < 4) return true;
-    else return false;
-  }
 
-  expandComponent(row) {
-    return (
-      <BSTable data={ row.expand } />
-    );
-  }
 
   render() {
+    console.log("Applied jobs", jobsApplied)
     const options = {
       expandRowBgColor: 'rgb(242, 255, 163)',
-      onlyOneExpanding: true
+      expandAll: true
     };
+
+
+
     return (
-      <BootstrapTable data={ products }
-        options={ options }
-        expandableRow={ this.isExpandableRow }
-        expandComponent={ this.expandComponent }
-        search>
-        <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
-        <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-        <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
-      </BootstrapTable>
+      <div>
+        <div className="col-md-12 panel" style={{ height: "50px", border: "1px solid gray" }}>
+          <h4>
+            <div className="col-md-3">JobId</div>
+            <div className="col-md-3">Designation</div>
+            <div className="col-md-3">Skills</div>
+            <div className="col-md-3">Location</div>
+          </h4>
+        </div>
+        <ul>
+          {jobsApplied.jobs.map(function (job) {
+            return (
+              <li key={job.jobID}>
+                <div>
+                  <UXComp job={job}></UXComp>
+                </div>
+              </li>
+            )
+          })}
+
+        </ul>
+      </div>
     );
+  }
+}
+
+
+class UXComp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeKey: '1'
+    }
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(activeKey) {
+    this.setState({ activeKey });
+  }
+  render() {
+    return (
+      <div>
+        <bootstrap.PanelGroup
+          accordion
+          id="accordion-controlled-example"
+          activeKey={this.state.activeKey}
+          onSelect={this.handleSelect}
+        >
+          <bootstrap.Panel eventKey="1">
+            <bootstrap.Panel.Heading>
+              <bootstrap.Panel.Title toggle>
+                <div className="col-md-3">{this.props.job.jobId}</div>
+                <div className="col-md-3">{this.props.job.Designation}</div>
+                <div className="col-md-3">{this.props.job.SkillSet}</div>
+                <div className="col-md-3">{this.props.job.Location}</div>
+               </bootstrap.Panel.Title>
+            </bootstrap.Panel.Heading>
+            <bootstrap.Panel.Body collapsible>Panel content 1</bootstrap.Panel.Body>
+          </bootstrap.Panel>
+        </bootstrap.PanelGroup>
+      </div>
+    )
   }
 }
